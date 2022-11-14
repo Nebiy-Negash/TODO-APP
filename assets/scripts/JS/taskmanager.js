c8e1a79cd7e9a05469529e08640a8f85ec6600c4
Nebiy Negash <neba048@gmail.com>
2022-11-14 16:56:41 -0600

    task 4 done empty input shows error

--- assets/scripts/JS/taskmanager.js
+++ assets/scripts/JS/taskmanager.js
@@ -1,11 +1,10 @@
 class TaskManager {
-    Constructor(name, description, assigned_to , due_date, status, currentId){
+    Constructor(name, description, assigned_to , due_date, status){
         this.name = name;
         this.description = description;
         this.assigned_to = assigned_to;
         this.due_date = due_date;
         this.status = status;
-        this.currentId = currentId;
         
     }
 
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,33 +1,44 @@
+// getting the input fields. through various document selection
+
 const newTaskNameInput = document.querySelector("#name-field");
-const newTaskDescription = document.getElementById("task-description-field")
-const newAssigned = document.getElementById("assigned-field")
-const newDueDate = document.getElementById("date-assign-field")
-const form = document.getElementById('task_form')
+const newTaskDescription = document.getElementById("task-description-field");
+const newAssigned = document.getElementById("assigned-field");
+const newDueDate = document.getElementById("date-assign-field");
+const form = document.getElementById('task_form');
 
-const newError = document.getElementById('name-error')
+// getting the error showing div element
 
+const nameError = document.getElementById('name-error');
+const descriptionError = document.getElementById('description-error')
+const assignError = document.getElementById('assign-error');
+
+// show error function for name input
+function showError(input, message) {
+        const displayError = input.parentElement;
+        displayError.className = 'form-control error-message';
+        const showErrorMessage = displayError.querySelector('small');
+        showErrorMessage.innerText = message;
+}
+
+
+// eventlistener
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+        e.preventDefault()
+
+        if(newTaskNameInput.value == '') {
+                showError(newTaskNameInput, 'Task name is required')
+        } else {
+                showError(newTaskNameInput, '');
+        }
+        if(newTaskDescription.value == '') {
+                showError(newTaskDescription, 'Task description is required')
+        } else {
+                showError(newTaskDescription, '');
+        }
+        if(newAssigned.value == '') {
+                showError(newAssigned, 'Task assignment is required')
+        } else {
+                showError(newAssigned, '');
+        }
 })
-
-function validFormFieldInput(data) {
-    const name = newTaskNameInput.value;
-    
-    if(name.value === '' || name.value == null){
-            newError.innerText = 'Task name is required'
-            return false
-    }  
-};
-
-
-const onSubmit = document.getElementById('btn');
-
-
-
-
-
-
-
-
-
 
